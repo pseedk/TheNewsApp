421e745abd6fbf3dab27876e9df66add52d252b0
pseedk <pseedk@ya.ru>
2022-10-26 16:41:41 +0300

    day-6

--- app/src/main/java/com/pseedk/thenewsapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/pseedk/thenewsapp/ui/search/SearchFragment.kt
@@ -1,19 +1,32 @@
 package com.pseedk.thenewsapp.ui.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.pseedk.thenewsapp.R
-import com.pseedk.thenewsapp.databinding.FragmentDetailsBinding
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pseedk.thenewsapp.databinding.FragmentSearchBinding
+import com.pseedk.thenewsapp.ui.adapters.NewsAdapter
+import com.pseedk.thenewsapp.utils.Resource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class SearchFragment : Fragment() {
-
 
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<SearchViewModel>()
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +34,51 @@ class SearchFragment : Fragment() {
     ): View {
         _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
+        var job: Job? = null
+        binding.edSearch.addTextChangedListener { text: Editable? ->  
+            job?.cancel()
+            job = MainScope().launch {
+                delay(500L)
+                text?.let {
+                    if (it.toString().isNotEmpty()) {
+                        viewModel.getSearchNews(query = it.toString())
+                    }
+                }
+            }
+        }
+        viewModel.searchNewsLiveData.observe(viewLifecycleOwner) { response ->
+            when(response) {
+                is Resource.Success -> {
+                    binding.pagSearchProgressBar.visibility = View.INVISIBLE
+                    response.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    binding.pagSearchProgressBar.visibility = View.INVISIBLE
+                    response.data?.let {
+                        Log.e("checkData", " MainFragment: error: ${it}")
+                    }
+                }
+                is Resource.Loading -> {
+                    binding.pagSearchProgressBar.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        binding.searchNewsAdapter.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
     override fun onDestroyView() {
